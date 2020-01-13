80ce6ef176c690e00df8f21a010aa18f268f9215
エッケザックス <kagekiyo7000@nifty.com>
2020-01-13 22:22:19 +0900

    Update github2codesandbox.js

--- github2codesandbox.js
+++ github2codesandbox.js
@@ -12,7 +12,7 @@ javascript: (() => {
     } else if (nowUrlArray[5] === "pull") {
         const innerHTML = document.getElementsByClassName("commit-ref css-truncate user-select-contain expandable head-ref")[0].innerHTML;
         //<a title="kagekiyo7/motocal:Separate-additionalDamage3" class="no-underline" href="/kagekiyo7/motocal/tree/Separate-additionalDamage3"><span class="css-truncate-target">kagekiyo7</span>:<span class="css-truncate-target">Separate-additionalDamage3</span></a>
-        githubUrl = innerHTML.match(/href=".+?"/)[0].match(/".+"/)[0].replace(/"\//, "").replace(/"/, "");
+        githubUrl = innerHTML.match(/href=".+?"/)[0].replace(/href="\/|"/g, "");
     } else {
         githubUrl = nowUrlArray.slice(3, 5).join("/");
     }
