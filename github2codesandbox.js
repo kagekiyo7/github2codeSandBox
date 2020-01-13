03d45c312b069c3df321cda6a157cb9794d8f8ce
エッケザックス <kagekiyo7000@nifty.com>
2020-01-13 21:47:25 +0900

    Update github2codesandbox.js

--- github2codesandbox.js
+++ github2codesandbox.js
@@ -5,23 +5,19 @@ javascript: (() => {
     }
         
     const nowUrlArray = location.href.split("/");
-    const codeSandBoxUrl = "https://codesandbox.io/s/github/";
     let githubUrl = "";
 
-    if (nowUrlArray[5] === "tree") {
+    if (nowUrlArray[5] === "tree" || nowUrlArray[5] === "blob") {
         githubUrl = nowUrlArray.slice(3, 7).join("/");
     } else if (nowUrlArray[5] === "pull") {
         const innerHTML = document.getElementsByClassName("commit-ref css-truncate user-select-contain expandable head-ref")[0].innerHTML;
         //<a title="kagekiyo7/motocal:Separate-additionalDamage3" class="no-underline" href="/kagekiyo7/motocal/tree/Separate-additionalDamage3"><span class="css-truncate-target">kagekiyo7</span>:<span class="css-truncate-target">Separate-additionalDamage3</span></a>
         githubUrl = innerHTML.match(/href=".+?"/)[0].match(/".+"/)[0].replace(/"\//, "").replace(/"/, "");
-    } else if (nowUrlArray.length === 5 || nowUrlArray.length === 6) {
-        githubUrl = nowUrlArray.slice(3, 5).join("/");
     } else {
-        alert("Unsupported URL");
-        return
+        githubUrl = nowUrlArray.slice(3, 5).join("/");
     }
     
-    window.open(codeSandBoxUrl + githubUrl, "_blank");
+    window.open("https://codesandbox.io/s/github/" + githubUrl);
     })();
     
    //https://github.com/kagekiyo7/motocal/tree/Separate-additionalDamage3
